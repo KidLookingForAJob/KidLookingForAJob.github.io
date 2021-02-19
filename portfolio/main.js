4b923b59c4f5b85cad4c7d3f2143f27257b8ebb7
KidLookingForAJob <79295900+KidLookingForAJob@users.noreply.github.com>
2021-02-18 23:13:31 -0600

    Add_Files
    Adding Files

---
+++ portfolio/main.js
@@ -0,0 +1,27 @@
+//Navbar change when in Mobile Mode
+let mainNav=document.getElementById('main-nav');
+let navbarToggle=document.getElementById('navbar-toggle');
+
+navbarToggle.addEventListener('click',function(){
+
+    if(this.classList.contains('active')){
+        mainNav.style.display="none";
+        this.classList.remove('active');
+    }
+    else{
+        mainNav.style.display="flex";
+        this.classList.add('active');
+
+    }
+});
+
+//Smooth Transition
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+     }, 500);
+});
+
+
